8f75587d6b4d210b3e41b6468617174fd04deb70
Maciej <mpek181@gmail.com>
2019-09-15 17:06:05 +0200

    init project

---
+++ src/main/java/com/mpek/ReservationSeat/ReservationSeatApplication.java
@@ -0,0 +1,13 @@
+package com.mpek.ReservationSeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationSeatApplication.class, args);
+	}
+
+}
